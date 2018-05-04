d757581c73f069c3393992dab83ce4c1b85c8a79
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2018-05-04 01:13:24 +0000

    updated gdelt for name search

--- src/spark_code/gdelt_df.py
+++ src/spark_code/gdelt_df.py
@@ -3,11 +3,20 @@ from pyspark import SparkContext ,SparkConf
 
 spark = SparkSession.builder.appName("gdelt").getOrCreate()
 conf = SparkConf().setAppName("gdelt")#.setMaster(master)
-#sc = spark.sparkContext()
+#sc = 
 
-lines = sc.textFile("s3a://gdelt-open-data/events/2018*") # Loads 73,385,698 records from 2016
+#lines = spark.sparkContext.textFile("s3a://gdelt-open-data/events/20180425*") 
+lines = spark.read.text("s3a://gdelt-open-data/v2/events/20160425*")
 # Split lines into columns; change split() argument depending on deliminiter e.g. '\t'
-parts = lines.map(lambda l: l.split('\t'))
+
+#parts = lines.map(lambda l: l.split('\t'))
+
+#numAs = lines.filter(lines.value.contains("TELEFONICA")).count()
+numAs =lines.filter(lines.value.contains("TELEFONICA")).show(20, False)
+
+print("Lines with a", numAs)
+
+'''
 # Convert RDD into DataFrame
 from urllib import urlopen
 html = urlopen("http://gdeltproject.org/data/lookups/CSV.header.dailyupdates.txt").read().rstrip()
@@ -15,5 +24,5 @@ columns = html.split('\t')
 df = spark.createDataFrame(parts, columns)
 df.printSchema()
 df.select("sqldate").show()
-sc.stop()
+'''
 spark.stop()
