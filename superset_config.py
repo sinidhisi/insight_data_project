d4d6f4729503d932a9730c19c154235b46283c39
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2018-05-15 04:38:45 +0000

    added superset config

---
+++ superset_config.py
@@ -0,0 +1,5 @@
+SUPERSET_WEBSERVER_ADDRESS = 'ec2-54-202-181-200.us-west-2.compute.amazonaws.com'
+SUPERSET_WEBSERVER_PORT = 8000
+PUBLIC_ROLE_LIKE_GAMMA = True
+#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/postgres'
+
